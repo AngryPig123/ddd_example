6c2368eb8271acc322a73948f03ef9295179568b
Angrypig123 <shguddnr3@naver.com>
2024-03-18 21:25:10 +0900

    first commit, project module setting, order root aggregateroot add

---
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/BaseId.java
@@ -0,0 +1,29 @@
+package com.food.ordering.system.domain.valueobject;
+
+import java.util.Objects;
+
+public abstract class BaseId<T> {
+    private final T t;
+
+    protected BaseId(T t) {
+        this.t = t;
+    }
+
+    public T getT() {
+        return t;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseId<?> baseId = (BaseId<?>) o;
+        return Objects.equals(t, baseId.t);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(t);
+    }
+
+}
